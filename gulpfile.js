6171aef517389055da96a3779ec9d7187cefc8d4
Robert Bethencourt <robert.bethencourt@gmail.com>
2018-05-26 13:18:41 -0400

    borwserSync now working

--- gulpfile.js
+++ gulpfile.js
@@ -1,20 +1,34 @@
 var gulp = require("gulp");
 const handlebars = require("gulp-compile-handlebars");
 const rename = require("gulp-rename");
-const concat = require("gulp-concat");
-const deporder = require("gulp-deporder");
-const stripdebug = require("gulp-strip-debug");
-const uglify = require("gulp-uglify");
+const browserSync = require("browser-sync").create();
 
+const paths = {
+  src: "./src/**/*",
+  html: "./src/*.handlebars",
+  partials: "./src/partials",
+  htmlDist: "./dist",
+  js: "./src/js/**/*",
+  jsDist: "./dist/js/",
+  css: "./src/css/**/*",
+  cssDist: "./dist/css/",
+  images: "./src/images/**/*",
+  imagesDist: "./dist/images/",
+  fonts: "./src/fonts/**/*",
+  fontsDist: "./dist/fonts/"
+};
+
+// handlebars files
+// convert the .handlebars files into .html files and output to the dist foler.
 gulp.task("html", () => {
   return gulp
-    .src("./src/*.handlebars")
+    .src(paths.html)
     .pipe(
       handlebars(
         {},
         {
           ignorePartials: true,
-          batch: ["./src/partials"]
+          batch: [paths.partials]
         }
       )
     )
@@ -23,43 +37,66 @@ gulp.task("html", () => {
         extname: ".html"
       })
     )
-    .pipe(gulp.dest("./dist"));
+    .pipe(gulp.dest(paths.htmlDist))
+    .pipe(
+      browserSync.reload({
+        stream: true
+      })
+    );
 });
 
+// javaScript files
 gulp.task("js", function() {
-  var jsBuild = gulp.src("./src/js/**/*");
-
-  /*** below code used if we want to combine all js files into one and minify them. Consider for production at end of project ***/
-  //   .pipe(deporder())
-  //   .pipe(concat("main.js"));
-  //
-  // if (!devBuild) {
-  //   jsBuild = jsBuild.pipe(stripdebug()).pipe(uglify());
-  // }
-
-  return jsBuild.pipe(gulp.dest("./dist/js/"));
+  var jsBuild = gulp.src(paths.js);
+  return jsBuild.pipe(gulp.dest(paths.jsDist)).pipe(
+    browserSync.reload({
+      stream: true
+    })
+  );
 });
 
+// css files
 gulp.task("css", function() {
-  /*** do I want to minify and uglify the css as well for production? ***/
-  var cssBuild = gulp.src("./src/css/**/*");
-  return cssBuild.pipe(gulp.dest("./dist/css/"));
+  var cssBuild = gulp.src(paths.css);
+  return cssBuild.pipe(gulp.dest(paths.cssDist)).pipe(
+    browserSync.reload({
+      stream: true
+    })
+  );
 });
 
+// browserSync allows us to reload the pages automatically when files are changed
+gulp.task("browserSync", function() {
+  browserSync.init({
+    server: {
+      baseDir: "dist"
+    }
+  });
+});
+
+// run gulp watch in root of the application so gulp watches for file changes and sends them to the dist folder
+gulp.task("watch", ["browserSync", "css"], function() {
+  gulp.watch(paths.css, ["css"]);
+  gulp.watch(paths.src, ["default"]);
+  gulp.watch(paths.src, browserSync.reload);
+});
+
+// the below will run when we run gulp watch, along with Browser Sync
+gulp.task("default", ["html", "js", "css"]);
+
+// production builds below
+
+// run the below without gulp watch, but with gulp imagea and gulp fonts. Used for production or when changing images, but don't need to rerun these task each time a change happens to a file.
+// image files
 gulp.task("images", function() {
-  /*** do I want to minify and uglify the css as well for production? ***/
   var imagesBuild = gulp.src("./src/images/**/*");
   return imagesBuild.pipe(gulp.dest("./dist/images/"));
 });
 
+// font files
 gulp.task("fonts", function() {
-  /*** do I want to minify and uglify the css as well for production? ***/
   var fontsBuild = gulp.src("./src/fonts/**/*");
   return fontsBuild.pipe(gulp.dest("./dist/fonts/"));
 });
 
-gulp.task("watch", function() {
-  gulp.watch("./src/**/*", ["default"]);
-});
-
-gulp.task("default", ["html", "js", "css", "images", "fonts"]);
+gulp.task("build", ["html", "js", "css", "images", "fonts"]);
